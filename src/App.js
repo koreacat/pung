a8cc2b808eeab0490999639e951db3ddf445487a
koreacat <kj9966@naver.com>
2021-08-21 21:54:25 +0900

    stores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import MessageStore from './service/messageStore.js';
+import MessageStore from './stores/messageStore.js';
+import MessageObserver from './observers/messageObserver.js';
 import Title from './components/title/Title.js';
 import MessageCreator from './components/messageCreator/MessageCreator.js';
 import MessageList from './components/messageList/MessageList.js';
 
 function App($app) {
     const init = () => {
-        const messageStore = new MessageStore();
+        const messageStore = new MessageStore(new MessageObserver());
         new Title({$target: $app, name: '펑 리스트'});
         new MessageCreator({$target: $app, messageStore});
         new MessageList({$target: $app});
--- src/stores/messageStore.js
+++ src/stores/messageStore.js
@@ -1,17 +1,15 @@
 import MessageVO from "../domain/MessageVO.js";
-import MessageObserver from "./messageObserver.js";
 
-function MessageStore() {
+function MessageStore(messageObserver) {
     this.snCount = 0;
     this.messageList = new Map();
-    this.messageObserver = new MessageObserver();
 
     this.createMessage = ({ text, time }) => {
         const sn = this.snCount;
         const messageVO = new MessageVO({ sn, text, createdDate: new Date(), time });
         this.messageList.set(sn, messageVO);
         this.snCount += 1;
-        this.messageObserver.ovserve(this.messageList);
+        messageObserver.ovserve(this.messageList);
         return messageVO;
     }
 
